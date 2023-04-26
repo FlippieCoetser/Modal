479cedbec99e52d26d6db55a08ad73ce35b061b5
Flippie.Coetser <flippie.coetser@gmail.com>
2023-04-26 10:26:02 -0700

    COMPONENT: Tag.MODAL IsDefined

--- test/Modal.test.ts
+++ test/Modal.test.ts
@@ -1,3 +1,14 @@
+// @category Metadata: Components
+import { Tag } from "../node_modules/@browser-modules/component.library/lib/tag.js";
+describe("Given Tag imported", () => {
+  it("then Tag should be defined", () => {
+    expect(Tag).toBeDefined();
+  });
+  it("then Tag.MODAL should be defined", () => {
+    expect(Tag.MODAL).toBeDefined();
+  });
+});
+
 import { test } from "../src/Modal.js";
 
 describe("Given Tag imported", () => {
