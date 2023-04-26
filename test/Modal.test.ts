2ddca56fcdded38d8cfd14ba19c567c54d3bc349
Flippie.Coetser <flippie.coetser@gmail.com>
2023-04-26 10:31:10 -0700

    META: Attribute Defined --> FAIL

--- test/Modal.test.ts
+++ test/Modal.test.ts
@@ -9,6 +9,25 @@ describe("Given Tag imported", () => {
   });
 });
 
+/**
+ * @category Metadata: Attributes
+ */
+import { Attribute } from "../src/Modal.metadata.js";
+describe("Given Attribute imported", () => {
+  it("then Attribute should be defined", () => {
+    expect(Attribute).toBeDefined();
+  });
+  it("then Attribute.TEMPLATE should be defined", () => {
+    expect(Attribute.TEMPLATE).toBeDefined();
+  });
+  it("then Attribute.VISIBLE should be defined", () => {
+    expect(Attribute.VISIBLE).toBeDefined();
+  });
+  it("then Attribute.STATE should be defined", () => {
+    expect(Attribute.STATE).toBeDefined();
+  });
+});
+
 import { test } from "../src/Modal.js";
 
 describe("Given Tag imported", () => {
