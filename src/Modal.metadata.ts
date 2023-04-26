dc1d2218530ef5ec5ebd99750c6f9c44ca7d5201
Flippie.Coetser <flippie.coetser@gmail.com>
2023-04-26 10:31:17 -0700

    META: Attribute Defined --> PASS

--- src/Modal.metadata.ts
+++ src/Modal.metadata.ts
@@ -1,3 +1,19 @@
 /**
  * @module Modal
  */
+
+/**
+ * HTML Attributes available to set
+ * @category Metadata: Attributes
+ * @enum
+ */
+export const Attribute = {
+  TEMPLATE: "templateId",
+  VISIBLE: "visible",
+  STATE: "state",
+} as const;
+/**
+ * HTML Attributes available to set
+ * @category Metadata: Attributes
+ */
+export type Attributes = (typeof Attribute)[keyof typeof Attribute];
